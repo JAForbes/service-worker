e107b5c9357478df53e2dff58fb3f84e1a0dc1e4
James <james.a.forbes@gmail.com>
2015-06-01 21:45:53 +1000

    Instead of having a local fallback, fetch a cloudfront image

--- sw.js
+++ sw.js
@@ -3,13 +3,13 @@ importScripts('serviceworker-cache-polyfill.js')
 self.oninstall = function(event) {
   self.skipWaiting();
 
-  event.waitUntil(
-    caches.open("v1").then(function(cache) {
-      return cache.addAll([
-        'fallback.jpg'
-      ]);
-    })
-  );
+  // event.waitUntil(
+  //   caches.open("v1").then(function(cache) {
+  //     return cache.addAll([
+  //       'https://dhsn5tcrzbqtk.cloudfront.net/1/large/5427196-e1341q.jpg'
+  //     ]);
+  //   })
+  // );
 };
 
 self.onfetch = function(event) {
@@ -17,7 +17,8 @@ self.onfetch = function(event) {
 
   if(event.request.url == "http://localhost/service-worker/awesome.jpg") {
 
-  	event.respondWith( caches.match("fallback.jpg") )
+
+  	event.respondWith( fetch("https://dhsn5tcrzbqtk.cloudfront.net/1/large/5427196-e1341q.jpg", { mode: "no-cors"}) )
 
   } else {
   	  //event.respondWith(  new Response('This came from the service worker!') )
